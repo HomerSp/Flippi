61086b334a11f781657f6b79df221dfcc6e5acbc
HomerSp <master.homer@gmail.com>
2017-08-06 19:12:11 +0200

    Fix typo in package name.

--- app/src/main/java/com/matnar/app/android/flippi/view/behavior/FooterBarBehavior.java
+++ app/src/main/java/com/matnar/app/android/flippi/view/behavior/FooterBarBehavior.java
@@ -1,12 +1,10 @@
-package com.matnar.app.android.flippi.view.behavoir;
+package com.matnar.app.android.flippi.view.behavior;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 
