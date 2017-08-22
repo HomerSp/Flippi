3b75e1919836fa070a51641be28d69157191c0ce
HomerSp <master.homer@gmail.com>
2017-08-22 12:11:41 +0200

    Fix rebase problem.

--- app/src/main/java/com/matnar/app/android/flippi/fragment/main/SearchResultFragment.java
+++ app/src/main/java/com/matnar/app/android/flippi/fragment/main/SearchResultFragment.java
@@ -300,6 +300,7 @@ public class SearchResultFragment extends MainActivity.MainActivityFragment {
         outState.putParcelable("categories", mCategories);
         outState.putString("filter", mFilter);
         outState.putLong("filter_category", mFilterCategory);
+        outState.putString("sort", mSort);
     }
 
     @Override
