4cda5dfed7f701a6fd04d1f3c458721e100f2ef0
HomerSp <master.homer@gmail.com>
2017-08-28 12:29:01 +0200

    Fix potential crash when exiting.

--- app/src/main/java/com/matnar/app/android/flippi/activity/MainActivity.java
+++ app/src/main/java/com/matnar/app/android/flippi/activity/MainActivity.java
@@ -315,9 +315,13 @@ public class MainActivity extends AppCompatActivity
 
         mPriceCheckDatabase.close();
 
-        if (mBillingHelper != null) {
-            mBillingHelper.dispose();
-            mBillingHelper = null;
+        try {
+            if (mBillingHelper != null) {
+                mBillingHelper.dispose();
+                mBillingHelper = null;
+            }
+        } catch(IllegalArgumentException e) {
+            // Empty
         }
     }
 
